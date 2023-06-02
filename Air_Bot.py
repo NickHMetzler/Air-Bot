3bfa212f42548b5b8af913348fbd8c97ad226afa
NickHMetzler <44005221+NickHMetzler@users.noreply.github.com>
2023-06-01 22:11:40 -0400

    Initial commit

---
+++ Air_Bot.py
@@ -0,0 +1,468 @@
+# Naval_Bot.py
+# Plays War Thunder Naval to automatically generate Silver Lions (In Game Currency)
+# 2023-06-01
+# Nicolas Metzler
+
+# Import Statements
+from pyautogui import *
+import pyautogui
+import time
+import keyboard
+import numpy as np
+import win32api, win32con
+import ctypes
+import random
+import os
+import threading
+
+# Allow the use of relative paths
+os.chdir(os.path.dirname(__file__))
+script_folder = os.path.dirname(__file__)
+# Change the permissions of all files in the script's folder to allow all users to read and write to them
+for file in os.listdir(script_folder):
+    file_path = os.path.join(script_folder, file)
+    os.chmod(file_path, 0o666)
+
+# PyAutoGui Failsafe off
+pyautogui.FAILSAFE = False
+
+game_stats = []
+
+# Char/Str to Scancode for Bot Game Inputs
+# https://kbdlayout.info/kbdusx/scancodes
+KEYS = {
+  'a': 0x1E,
+  'b': 0x30,
+  'c': 0x2E,
+  'd': 0x20,
+  'e': 0x12,
+  'f': 0x21,
+  'g': 0x22,
+  'h': 0x23,
+  'i': 0x17,
+  'j': 0x24,
+  'k': 0x25,
+  'l': 0x26,
+  'm': 0x32,
+  'n': 0x31,
+  'o': 0x18,
+  'p': 0x19,
+  'q': 0x10,
+  'r': 0x13,
+  's': 0x1F,
+  't': 0x14,
+  'u': 0x16,
+  'v': 0x2F,
+  'w': 0x11,
+  'x': 0x2D,
+  'y': 0x15,
+  'z': 0x2C,
+  '1': 0x02,
+  '2': 0x03,
+  '3': 0x04,
+  '4': 0x05,
+  '5': 0x06,
+  '6': 0x07,
+  '7': 0x08,
+  '8': 0x09,
+  '9': 0x0A,
+  '0': 0x0B,
+  'enter': 0x1C,
+  'esc': 0x01,
+  'backspace': 0x0E,
+  'tab': 0x0F,
+  ' ': 0x39,
+  '-': 0x0C,
+  '=': 0x0D,
+  '[': 0x1A,
+  ']': 0x1B,
+  '\\': 0x2B,
+  '#': 0x0E,
+  ';': 0x27,
+  '\'': 0x28,
+  '`': 0x29,
+  ',': 0x33,
+  '.': 0x34,
+  '/': 0x35,
+  'caps': 0x3A,
+  'l_shift' : 0x2A,
+  'f1': 0x3B,
+  'f2': 0x3C,
+  'f3': 0x3D,
+  'f4': 0x3E,
+  'f5': 0x3F,
+  'f6': 0x40,
+  'f7': 0x41,
+  'f8': 0x42,
+  'f9': 0x43,
+  'f10': 0x44,
+  'f11': 0x57,
+  'f12': 0x58}
+
+# Get User's KeyBinds from file
+with open('keybinds.txt', 'r') as file:
+    # Read the contents of the file
+    contents = file.read()
+
+# Evaluate the contents as Python code
+KEYBINDS = eval(contents)
+
+
+# Phrases that can be typed in chat
+phrase_count = -1
+with open('chat_phrases.txt', 'r') as file:
+    # create an empty list
+    CHATPHRASES = []
+    # iterate over the lines in the file
+    for line in file:
+        # strip leading and trailing whitespace from the line
+        line = line.strip()
+        if line and not line.startswith('#'):
+            # add the line to the list
+            CHATPHRASES.append(line)
+            phrase_count += 1
+
+
+###############################
+#   C struct redefinitions    #
+###############################
+PUL = ctypes.POINTER(ctypes.c_ulong)
+class KeyBdInput(ctypes.Structure):
+    _fields_ = [("wVk", ctypes.c_ushort),
+                ("wScan", ctypes.c_ushort),
+                ("dwFlags", ctypes.c_ulong),
+                ("time", ctypes.c_ulong),
+                ("dwExtraInfo", PUL)]
+
+class HardwareInput(ctypes.Structure):
+    _fields_ = [("uMsg", ctypes.c_ulong),
+                ("wParamL", ctypes.c_short),
+                ("wParamH", ctypes.c_ushort)]
+
+class MouseInput(ctypes.Structure):
+    _fields_ = [("dx", ctypes.c_long),
+                ("dy", ctypes.c_long),
+                ("mouseData", ctypes.c_ulong),
+                ("dwFlags", ctypes.c_ulong),
+                ("time",ctypes.c_ulong),
+                ("dwExtraInfo", PUL)]
+
+class Input_I(ctypes.Union):
+    _fields_ = [("ki", KeyBdInput),
+                 ("mi", MouseInput),
+                 ("hi", HardwareInput)]
+
+class Input(ctypes.Structure):
+    _fields_ = [("type", ctypes.c_ulong),
+                ("ii", Input_I)]
+
+def pressKey(hexKeyCode):
+    extra = ctypes.c_ulong(0)
+    ii_ = Input_I()
+    ii_.ki = KeyBdInput( 0, hexKeyCode, 0x0008, 0, ctypes.pointer(extra) )
+    x = Input( ctypes.c_ulong(1), ii_ )
+    ctypes.windll.user32.SendInput(1, ctypes.pointer(x), ctypes.sizeof(x))
+
+def releaseKey(hexKeyCode):
+    extra = ctypes.c_ulong(0)
+    ii_ = Input_I()
+    ii_.ki = KeyBdInput( 0, hexKeyCode, 0x0008 | 0x0002, 0, 
+ctypes.pointer(extra) )
+    x = Input( ctypes.c_ulong(1), ii_ )
+    ctypes.windll.user32.SendInput(1, ctypes.pointer(x), ctypes.sizeof(x))
+
+
+#################
+#   Functions   #
+#################
+
+# End Program
+def end_program():
+    # Send the signal to terminate the program
+    os.kill(os.getpid(), 9)
+
+def move_mouse_randomly():
+  # Move the mouse randomly a few times
+  for _ in range(random.randint(3, 6)):
+    move_mouse_by(x=random.randint(-250, 250), y=random.randint(-10, 15))
+    # Wait a little before moving the mouse again
+    time.sleep(random.uniform(0.1, 0.5))
+
+def click_mouse():
+    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN, 0, 0) 
+    time.sleep(np.random.uniform(0.3,0.5)) 
+    win32api.mouse_event(win32con. MOUSEEVENTF_LEFTUP, 0, 0)
+
+# Chat a random phrase
+def random_chat():
+    # Pick a Random message
+    message = random.choice(CHATPHRASES)
+    print(f'CONSOLE: Typing Message : {message}')
+    press('enter')
+    time.sleep(np.random.uniform(0.3,0.7)) 
+    for letter in message:
+        if letter.isupper() == True:
+            press('caps')
+            type(letter.lower())
+            press('caps')
+        else:
+            type(letter)
+    press('enter')
+
+# Move mouse to given X, Y Coordinates
+def move_mouse_to(x, y):
+    pyautogui.moveTo(x, y)
+
+# Move mouse by a given X, Y Value
+def move_mouse_by(x, y):
+    win32api.mouse_event(win32con.MOUSEEVENTF_MOVE, x, y, 0, 0)
+
+# Click the provided button
+def click_button(image_path):
+    button = pyautogui.locateCenterOnScreen(image_path, grayscale=False, confidence=0.75)
+    if button != None:
+        move_mouse_to(button[0], button[1])
+        time.sleep(np.random.uniform(0.5,0.7))
+        click(button[0], button[1])
+        button = pyautogui.locateCenterOnScreen(image_path, grayscale=False, confidence=0.75)
+        return True
+    else:
+        return False
+
+# Syntax is lowercase
+# Hold a given key
+def hold(key):
+    pressKey(KEYS[key])
+
+# Release a given key
+def release(key):
+    releaseKey(KEYS[key])
+
+# Press a given Key
+def press(key):
+    hold(key)
+    time.sleep(np.random.uniform(0.3,0.7)) 
+    release(key)
+
+# Hold a given key for a specified amount of time
+def holdFor(key, seconds):
+    hold(key)
+    time.sleep(seconds) 
+    release(key)
+
+# Type a given key
+# Function is faster, designed for typing messages
+def type(key):
+    hold(key)
+    time.sleep(np.random.uniform(0.1,0.3)) 
+    release(key)
+
+# Function to calculate the elapsed time
+def get_elapsed_time(startTime):
+    current_time = time.time()
+    elapsed_time = current_time - startTime
+    return elapsed_time
+
+# Bot Loop
+def bot():
+    while True:
+
+        # Click 'To Battle' Button in Main Menu
+        while pyautogui.locateOnScreen('assets/in_queue.png', grayscale=False, confidence=0.75) == None:
+            press(KEYBINDS['enter'])
+            time.sleep(0.3)
+        print("CONSOLE: To Battle!")
+
+        # Wait to Join Battle
+        print(f'CONSOLE: Waiting in Qeue...')
+        while pyautogui.locateCenterOnScreen('assets/spawn.png', grayscale=False, confidence=0.8) == None:
+            pass
+        
+        # In Battle, click the Spawn In button
+        press(KEYBINDS['enter'])
+        print("CONSOLE: Spawn Button Clicked") 
+
+        time.sleep(np.random.uniform(0.5,0.7))
+
+        # Wait to Spawn in
+        print('CONSOLE: Waiting to Spawn In')
+        while pyautogui.locateOnScreen('assets/cancel.png', grayscale=False, confidence=0.7) != None:
+            pass
+
+        # Get current time
+        battle_time = time.time()
+
+        # Picth values
+        pitch_value = 368
+        downVal = int(pitch_value/92)
+
+        # Throttle up, then pitch up
+        holdFor('w', 4)
+        move_mouse_by(0, -pitch_value)
+        
+        # Start CCRP and choose base
+        print('CONSOLE: Activating CCRP')
+        press(KEYBINDS['ccrp'])
+
+        while get_elapsed_time(battle_time) < 29.0:
+            pass
+        # Retract gear
+        press(KEYBINDS['gear'])
+
+        while get_elapsed_time(battle_time) < 42.0:
+            pass
+
+        # Slowly pitch the plane back to level
+        while get_elapsed_time(battle_time) < 88.0:
+            move_mouse_by(0, downVal)
+            time.sleep(1)
+
+        # 60% chance that the bot will Chat this game
+        choice = random.randint(0, 10)
+        if choice <= 6 and phrase_count >= 0:
+            chat_flag = False
+            # Set the Chat to All
+            press('enter')
+            time.sleep(np.random.uniform(1.2,1.7))
+            press('tab')
+            time.sleep(np.random.uniform(1.2,1.7))
+            press('enter')
+        else:
+            chat_flag = True
+
+        # For testing purposes 
+        chat_flag = True
+
+        # Set variables for game loop
+        battle_time = time.time()
+        time_report = 0
+        zoom_time = 0
+        zoom_flag = False
+        hold(KEYBINDS['bomb'])
+
+        # Bombing loop
+        while pyautogui.locateOnScreen('assets/return_to_hangar.png', grayscale=False, confidence=0.7) == None and pyautogui.locateOnScreen('assets/to_hangar.png', grayscale=False, confidence=0.7) == None:
+            
+            # Report the time every 20 cycles
+            if time_report >= 20:
+                time_report = 0
+            elif time_report == 0:
+                # Print how long the bot ahs been in a match
+                current_time = time.time()
+                minutes, seconds = divmod(int(current_time - battle_time), 60)
+                if seconds <= 9:
+                    seconds = '0' + str(seconds)
+                print(f'CONSOLE: Playing Game...\nCONSOLE: Elapsed Time : {minutes}:{seconds}')
+            time_report += 1
+            
+            # Type a message in chat
+            choice = random.randrange(0, phrase_count)
+            if chat_flag == False:
+                chat_flag = True
+                random_chat()
+
+            # Locate the CCRP line and move the mouse towards it
+            location = pyautogui.locateOnScreen('assets/baseline.png', grayscale=False, confidence=0.6)
+            if location is not None:
+                center_x, center_y= pyautogui.center(location)
+                
+                # Get the screen's center coordinates
+                screen_width, screen_height = pyautogui.size()
+                screen_center_x = screen_width // 2
+                
+                # Calculate the distance between the target center and the screen's center
+                distance_x = int((center_x - screen_center_x)/5)
+                move_mouse_by(distance_x, 0)
+            else:
+                release(KEYBINDS['bomb'])
+                break
+
+            # Wait 10 cycles before zooming in
+            if zoom_time < 10 or zoom_flag == False:
+                zoom_time += 1
+            else:
+                press(KEYBINDS['zoom'])
+                zoom_flag = True
+
+        #  After Bombing pitch up and bait enemies
+        press(KEYBINDS['smoke'])
+        move_mouse_by(0, -200)
+        while pyautogui.locateOnScreen('assets/return_to_hangar.png', grayscale=False, confidence=0.7) == None and pyautogui.locateOnScreen('assets/to_hangar.png', grayscale=False, confidence=0.7) == None:
+            move_mouse_by(5, 0)
+            time.sleep(np.random.uniform(1.2,1.7))
+
+        # Vehicle has been destroyed
+        if pyautogui.locateCenterOnScreen('assets/return_to_hangar.png', grayscale=False, confidence=0.75) != None:
+            print('CONSOLE: Aircraft Downed, Returning to Hangar')
+            # Click 'Return To Hangar' Button
+            clicked = False
+            while clicked == False:
+                clicked = click_button('assets/return_to_hangar.png')
+            time.sleep(np.random.uniform(20,30))
+
+            # Click 'To Hangar' Button
+            clicked = False
+            while clicked == False:
+                clicked = click_button('assets/to_hangar.png')
+        # Match has ended
+        else:
+            print('CONSOLE: Match Ended, Returning to Hangar')
+            # Click 'To Hangar' Button
+            clicked = False
+            while clicked == False:
+                clicked = click_button('assets/to_hangar.png')
+
+
+title_banner = """
+  _   _ ___ ____ _  ______   __        ___    ____    _____ _   _ _   _ _   _ ____  _____ ____    ____   ___ _____   _   _ 
+ | \ | |_ _/ ___| |/ / ___|  \ \      / / \  |  _ \  |_   _| | | | | | | \ | |  _ \| ____|  _ \  | __ ) / _ \_   _| / | / |
+ |  \| || | |   | ' /\___ \   \ \ /\ / / _ \ | |_) |   | | | |_| | | | |  \| | | | |  _| | |_) | |  _ \| | | || |   | | | |
+ | |\  || | |___| . \ ___) |   \ V  V / ___ \|  _ <    | | |  _  | |_| | |\  | |_| | |___|  _ <  | |_) | |_| || |   | |_| |
+ |_| \_|___\____|_|\_\____/     \_/\_/_/   \_\_| \_\   |_| |_| |_|\___/|_| \_|____/|_____|_| \_\ |____/ \___/ |_|   |_(_)_|
+                                                                                                                           
+                                                                                                                    
+"""
+# Main function
+def main():
+    # Main menu
+    print(title_banner)
+    print('Welcome to Nicks War Thunder Air Bot 1.1\nThis program is designed to help you generate silver lions AFK in War Thunder.\n\nSetup Instructions : \n1. Check the KeyBinds file and ensure that your keybinds are set up correctly\n2. Go to Hangar and have the Kfir Canard (Israel) selected \n3. Select "Air Realistic Battles"\n\nTo end the program press and hold "q" at any time')
+
+    while True:
+        # Prompt the user for input
+        user_input = input("\nPlease press Enter to execute program : ")
+
+        # If the user presses enter, execute the program
+        if user_input == "":
+            time.sleep(2)
+            break
+        elif user_input == 'q' or user_input == 'Q':
+            end_program()
+        else:
+            print('CONSOLE: Error, "Enter" (Start Bot) or "q" (Quit Program) were not inputted')
+
+
+    # Allow time for User to Alt-Tab
+    print('\nCONSOLE: Starting Program...')
+    print('Please Alt + Tab to War Thunder\n')
+    for i in range(5):
+        print(f'CONSOLE: Starting Bot in {5 - i} Seconds...')
+        time.sleep(1)
+
+    # Create a thread for the bot
+    bot_thread = threading.Thread(target=bot)
+
+    # Start the bot
+    bot_thread.start()
+
+    # Check if the user presses key 'q' then quit the program
+    while True:
+        if keyboard.is_pressed('q'):
+            # handle the 'q' key press
+            print("CONSOLE: Exiting program")
+            # Quit the program
+            end_program()
+
+
+# Start the main thread
+main()
---
+++ ReadMe.md
@@ -0,0 +1,42 @@
+War Thunder Bot
+
+This is a War Thunder Bot designed to play the Kfir Canard automatically. It automatically plays the game and bombs bases to generate silver lions and research points. Please follow the instructions below to set up and use the bot effectively.
+
+Installation
+
+    Download the latest release of the War Thunder Bot from the GitHub repository.
+    Extract the downloaded files to a convenient location on your computer.
+
+Setup
+
+    Open the keybinds.txt file in a text editor.
+    Review the default keybinds provided and modify them as needed to match your preferred settings in War Thunder.
+    Save the keybinds.txt file.
+
+Chat Phrases Configuration
+
+    Open the chatphrases.txt file in a text editor.
+    Customize the chat phrases that the bot will send during gameplay.
+    Each phrase should be written on a new line.
+    Save the chatphrases.txt file.
+
+Usage
+
+    Run the executable or Python file as Administrator to avoid any errors.
+    Follow the on-screen instructions to start the bot.
+    The bot will automatically start playing the game for you.
+    During gameplay, the bot will periodically send chat messages according to the configured phrases.
+    To stop the bot, press and hold the q key for a second.
+
+Important Notes
+
+    The War Thunder Bot is intended for personal use only. Do not use it for any malicious or unfair purposes.
+    Ensure that you have the latest version of War Thunder installed on your computer for optimal compatibility.
+
+Disclaimer
+
+The War Thunder Bot is a third-party tool and is not officially affiliated with or endorsed by Gaijin Entertainment, the developer of War Thunder. The use of this bot is at your own risk, and the developers and contributors are not responsible for any consequences resulting from its usage.
+
+Contributions
+
+Contributions to the War Thunder Bot are welcome! If you encounter any issues or have suggestions for enhancements, please submit them via the GitHub repository's issue tracker.
